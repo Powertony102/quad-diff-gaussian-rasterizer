2bef38ebde972d63bf963c50d6a1888d293e3473
Powertony102 <powertony102@gmail.com>
2025-07-25 23:10:21 +0800

    Fix 0 tensor-1

--- diff_gaussian_rasterization/__init__.py
+++ diff_gaussian_rasterization/__init__.py
@@ -210,18 +210,18 @@ class GaussianRasterizer(nn.Module):
         
         # Ensure all parameters are proper tensors, not None or boolean values
         if shs is None or not torch.is_tensor(shs):
-            shs = torch.tensor([], device=means3D.device, dtype=means3D.dtype)
+            shs = torch.Tensor([])
         if colors_precomp is None or not torch.is_tensor(colors_precomp):
-            colors_precomp = torch.tensor([], device=means3D.device, dtype=means3D.dtype)
+            colors_precomp = torch.Tensor([])
         if opacities is None:
             raise Exception('opacities parameter cannot be None!')
 
         if scales is None or not torch.is_tensor(scales):
-            scales = torch.tensor([], device=means3D.device, dtype=means3D.dtype)
+            scales = torch.Tensor([])
         if rotations is None or not torch.is_tensor(rotations):
-            rotations = torch.tensor([], device=means3D.device, dtype=means3D.dtype)
+            rotations = torch.Tensor([])
         if cov3D_precomp is None or not torch.is_tensor(cov3D_precomp):
-            cov3D_precomp = torch.tensor([], device=means3D.device, dtype=means3D.dtype)
+            cov3D_precomp = torch.Tensor([])
 
         # );
         # Invoke C++/CUDA rasterization routine
